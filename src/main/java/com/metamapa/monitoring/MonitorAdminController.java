2fc96ac0c9012098a6af9ad897e6f7b0971d725a
Aponte Lucas <laponte@frba.utn.edu.ar>
2026-02-14 20:50:11 -0300

    monitoreoHecho

---
+++ src/main/java/com/metamapa/monitoring/MonitorAdminController.java
@@ -0,0 +1,46 @@
+package com.metamapa.monitoring;
+
+import com.metamapa.monitoring.healthindicators.DatabaseHealthIndicator;
+import com.metamapa.monitoring.healthindicators.agregadorHealthIndicator;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/monitor")
+public class MonitorAdminController {
+
+    private final DatabaseHealthIndicator database;
+    private final agregadorHealthIndicator agregador;
+
+
+    public MonitorAdminController(
+            DatabaseHealthIndicator database,
+            agregadorHealthIndicator agregador) {
+
+        this.database = database;
+        this.agregador = agregador;
+    }
+
+    // ---------- FALLAS ----------
+
+    @PostMapping("/fail/database")
+    public void failDatabase() {
+        database.markDown();
+    }
+
+    @PostMapping("/fail/fuente-dinamica")
+    public void failFuenteDinamica() {
+        agregador.markDown();
+    }
+
+
+    // ---------- RECUPERACIÓN ----------
+
+    @PostMapping("/recover/all")
+    public void recoverAll() {
+        database.markUp();
+        agregador.markUp();
+    }
+}
